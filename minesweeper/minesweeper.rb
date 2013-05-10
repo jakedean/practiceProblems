ed34d266177040340a2790b7e6139d284d77ba59
Jake Dean <jbd74@cornell.edu>
2013-05-10 15:46:06 -0400

    finished basic game

--- minesweeper/minesweeper.rb
+++ minesweeper/minesweeper.rb
@@ -14,11 +14,11 @@ be over and the board will be showed.
 class Minesweeper
 
   attr_accessor :size, :mines, :player_view, :master_view, :make_player_view, 
-  :make_master_view, :mine_creator, :both_views, :display_player_view
+  :make_master_view, :mine_creator, :both_views, :display_view, :player_move
   
   def initialize(size)
   	@size = size
-  	@mines = 2*size
+  	@mines = size
   	@both_views = []
   	@counter = 0
   	@ripple_arr = []
@@ -59,7 +59,7 @@ class Minesweeper
   	return @master_view
   end
 
-  def display_player_view (index)
+  def display_view (index)
   	full_array = []
     for x in 1..@size
     	line_arr = []
@@ -108,25 +108,26 @@ class Minesweeper
 		return @master_view
   end
 
-  def player_move 
-  	coulumn = validate_column
-  	row = validate_row
+  def player_move
+    row = validate_row 
+  	column = validate_column
   	if @master_view[row][column] == "M"
-  		uncover(row, column, 1)
+      uncover(row, column, 0)
+      puts "Oh no you have hit a mine, here is your board"
+      display_view(0)
   		game_over
-  	elsif @master[row][column] != 0
-  		uncover(row, column)
+  	elsif @master_view[row][column] != 0
+  		uncover(row, column, 1)
   	else
-  		ripple(row, coulmn)
+  		ripple(row, column)
   	end
   end
 
   def validate_column
   	puts "Can you give me the number of a column you would like to explore from 1 to #{@size}"
-  	potential_column = gets.chomp
-  	if potential_column.is_a(Integer) && potential_column.in(1..@size)
-  		column = potential_column
-  		return coulmn
+  	column = gets.chomp.to_i
+  	if column.is_a?(Integer) && (1..@size).include?(column)
+  		return column
   	else
   		puts "Not going to work, let's try again"
   		validate_column
@@ -135,61 +136,110 @@ class Minesweeper
 
   def validate_row
   	puts "Can you give me a number of the row you would like to explore from 1 to #{@size}"
-  	potential_row = gets.chomp
-  	if potential_row.is_a(Integer) && potential_row.in(1..@size)
-  		return potential_row
+  	row = gets.chomp.to_i
+  	if row.is_a?(Integer) && (1..@size).include?(row)
+  		return row
   	else
   		validate_row
   	end
   end
 
+  def is_game_over
+    for x in 1..@size
+      for y in 1..@size
+        return
+        if @player_view[x][y] == "H"
+          next
+        else
+          return true
+        end
+      end
+      if x == @size 
+        return false
+      else
+        next
+      end
+    end
+  end
 
   def uncover (row, column, call_or_not)
     holder = @master_view[row][column]
     @player_view[row][column] = holder
+    puts 'call or not'
+    puts call_or_not
     if call_or_not == 1
-    	display_player_view(0)
+      display_view(0)
+      if is_game_over == true
+        game_over
+      else
+        player_move
+      end
     end
   end
 
-  def ripple_checker (row, column) 
-		if @master_view[row][column] == 0
-      @ripple_arr.push([row,column])
+  def ripple_checker (row, column)
+    puts 'in the ripple checker'
+    if @master_view[row] == nil
+      return false
+    elsif @master_view[row][column] == nil
+      return false 
+		elsif @master_view[row][column] == 0
       return true
-    end
+    else
+      return false
+    end  
+  end
+
+  def ripple_adder (row, column)
+    @ripple_arr.push([row,column])
   end
 
   def ripple (row, column)
-    if @master_view[(row+@neg_ripple_counter)][column] == 0
-    	while ripple_checker((row+@neg_ripple_counter), column) == true do
-    		ripple_checker((row+@neg_ripple_counter), column)
+    @ripple_arr = []
+    @ripple_arr.push([row, column])
+    if @master_view[(row + @neg_ripple_counter)] != nil &&
+      @master_view[(row + @neg_ripple_counter)][column] == 0
+      puts 'in the first one'
+    	until ripple_checker((row + @neg_ripple_counter), column) == false do
+    		puts 'got here 1'
+        ripple_adder((row + @neg_ripple_counter), column)
         @neg_ripple_counter -= 1
       end
       @neg_ripple_counter = -1
     end
-    if @master_view[(row+@pos_ripple_counter)][column] == 0
-    	while ripple_checker((row+@pos_ripple_counter), column) == true do
-    		ripple_checker((row+@pos_ripple_counter), column)
+    if @master_view[(row + @pos_ripple_counter)] != nil &&
+      @master_view[(row + @pos_ripple_counter)][column] == 0
+        puts 'in the second one' 
+      until ripple_checker((row + @pos_ripple_counter), column) == false do
+        puts 'got here2'
+    		ripple_adder((row + @pos_ripple_counter), column)
         @pos_ripple_counter += 1
       end
-      @neg_ripple_counter = 1
+      @pos_ripple_counter = 1
     end
-    if @master_view[row][(column+@neg_ripple_counter)] == 0
-    	while ripple_checker((row+@neg_ripple_counter), column) == true do
-    		ripple_checker((row+@neg_ripple_counter), column)
+    if @master_view[row] != nil &&
+      @master_view[row][(column + @neg_ripple_counter)] == 0
+      puts 'in the hthird one'
+    	until ripple_checker(row, (column + @neg_ripple_counter)) == false do
+    		puts 'got here 3'
+        ripple_adder(row, (column + @neg_ripple_counter))
         @neg_ripple_counter -= 1
       end
       @neg_ripple_counter = -1
     end
-    if @master_view[row][(column+@pos_ripple_counter)] == 0
-    	while ripple_checker((row+@pos_ripple_counter), column) == true do
-    		ripple_checker((row+@pos_ripple_counter), column)
+    if @master_view[row] != nil && 
+      @master_view[row][(column + @pos_ripple_counter)] == 0
+      puts 'in the forth one'
+    	until ripple_checker(row, (column + @pos_ripple_counter)) == false do
+    		puts 'got here 4'
+        ripple_adder(row, (column + @pos_ripple_counter))
         @pos_ripple_counter += 1
       end
-      @neg_ripple_counter = 1
+      @pos_ripple_counter = 1
     end
     for x in 0...@ripple_arr.length
-    	if x < (@ripple_arr - 1)
+      puts @ripple_arr.to_s
+    	if x < (@ripple_arr.length - 1)
     		uncover(@ripple_arr[x][0], @ripple_arr[x][1], 0)
     	else
     		uncover(@ripple_arr[x][0], @ripple_arr[x][1], 1)
@@ -198,16 +248,17 @@ class Minesweeper
   end
 
   def game_over 
-  	puts "You have hit a mine, here was your board here was the master board"
-  	display_player_view(1)
+  	puts ".... and here was the master board"
+  	display_view(1)
   end
 
 end
-
 
 my_minesweeper = Minesweeper.new(10)
 my_minesweeper.make_player_view
 my_minesweeper.make_master_view
 master_view, player_view = my_minesweeper.mine_creator
-my_minesweeper.display_player_view
-puts my_minesweeper.fill_in_master
+my_minesweeper.display_view(0)
+my_minesweeper.fill_in_master
+my_minesweeper.display_view(1)
+my_minesweeper.player_move
